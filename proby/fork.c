caf08f1b06a8233c17f48f5e66a02568103ead88
aamarei <rribatullin@mail.ru>
2021-06-10 15:09:08 +0300

    proby

--- proby/fork.c
+++ proby/fork.c
@@ -8,36 +8,72 @@
 
 int main(int ac, char **av, char **envp)
 {
-	pid_t	pid1;
+	pid_t	pid[2];
 	pid_t	pid2;
 	char	c;
 	int		n;
 	int		i;
 	int		j;
 	int		status;
+	char **arg;
+	int		fd[5][2];
+	int stat;
 
-	pid1 = fork();
-	pid2 = fork();
-	if (pid1 != 0)
+	arg = (char **)malloc(sizeof(char *) * 4);
+	arg[0] = (char *)malloc(sizeof(char) * 10);
+	arg[1] = (char *) malloc(sizeof(char) * 20);
+	arg[2] = NULL;
+
+	arg[0] = "echo";
+	arg[1] = "Attempt at writing";
+
+	//execve("/bin/echo", arg, envp);
+
+	pipe(fd[0]);
+
+	pid[0] = fork();
+	//pipe(fd[1]);
+	//pid2 = fork();
+	if (pid[0] == 0)
 	{
-		if (pid2 != 0)
-			c = 'X';
-		else
-			c = 'Z';
-		//printf("n = %d, \t pid = %d, \t ppid = % d \n", n, getpid(), getppid());
-		//kill(pid, SIGKILL);
+		dup2(fd[0][1], 1);
+		close(fd[0][0]);
+		close(fd[0][1]);
+		execve("/bin/ech", arg, envp);
+			exit(10);
 	}
-	else
+	//else
+	//{
+		pid[1] = fork();
+		if (pid[1] == 0)
+		{
+			char *cmd[] = {"cat", "-e", NULL};
+
+			arg[0] = "cat";
+			arg[1] = "-e";
+
+			dup2(fd[0][0], 0);
+
+			close(fd[0][0]);
+			close(fd[0][1]);
+			execve("/bin/ca", cmd, envp);
+			exit(-10);
+		}
+		close(fd[0][0]);
+		close(fd[0][1]);
+			//wait(&stat);
+	//}
+	int h = -1;
+	stat = 0;
+	while (stat == 0 && ++h < 2)
 	{
-		if (pid2 != 0)
-			c = 'Y';
-		else
-			c = 'W';
-		//printf("n = %d, \t pid = %d, \t ppid = % d \n", n, getpid(), getppid());
-		//kill(pid, SIGKILL);
+		waitpid(pid[h], &stat, 0);
+		stat = WEXITSTATUS(stat);
+		printf("h = %d\n", h);
 	}
-	//wait(&status);
-	//kill(pid2, SIGKILL);
-	printf("%c \n", c);
+	//wait(&pid1);
+	printf("stats %d\n", stat);
+	printf("Ok\n");
+	//while (1);
 	return (0);
 }
